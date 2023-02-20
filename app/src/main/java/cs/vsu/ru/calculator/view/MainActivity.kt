1c9af1fae1d87be9039a77784806f278e93adfe5
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-02-20 19:51:09 +0300

    Init commit

---
+++ app/src/main/java/cs/vsu/ru/calculator/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package cs.vsu.ru.calculator.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.vsu.ru.calculator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
